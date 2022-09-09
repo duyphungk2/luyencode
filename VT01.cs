7f9548a0e7e0cfc485c8acd133ec2de90f00086a
Phùng Đặng Duy <91354013+duyphungk2@users.noreply.github.com>
2022-09-09 13:56:28 +0700

    Update VT01.cs

--- VT01.cs
+++ VT01.cs
@@ -1,23 +1,22 @@
 using System;
-namespace Ngay2
+
+namespace baiVT01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string s = Console.ReadLine();
-            var s1 = s.Split(' ');
-            int a = Convert.ToInt32(s1[0]);
-            int b = Convert.ToInt32(s1[1]);
-            Console.WriteLine(ucln(a, b));
-        }
-            static int ucln(int x, int y)
-        {
-            if ((x == 0) && (y != 0)) return Math.Abs(y);
-            if ((y == 0) && (x != 0)) return Math.Abs(x);
-            if (x == y) return Math.Abs(y);
-            if (Math.Abs(x)> Math.Abs(y)) return ucln(Math.Abs(x) - Math.Abs(y), Math.Abs(y));
-            else return ucln(Math.Abs(y)- Math.Abs(x), Math.Abs(x));
+            var s = Console.ReadLine();
+            int n = Convert.ToInt32(s);
+            var s1 = Console.ReadLine();
+            var s2 = s1.Split(' ');
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = Convert.ToInt32(s2[i]);
+            int max = a[0];
+            for (int i = 0; i < n; i++)
+                if (max < a[i]) max = a[i];
+            Console.WriteLine(max);
         }
     }
 }
